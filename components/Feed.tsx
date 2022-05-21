9fc21e2c5c14020c435ee1c434997d90b8b01f37
Gytis <gytissnei@gmail.com>
2022-05-21 12:06:48 +0300

    Initialized "Sanity"

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,13 +1,19 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import TweetBox from './TweetBox'
 
 function Feed() {
   return (
-    <div className='col-span-8 lg:col-span-5'>
+    <div className='col-span-8 lg:col-span-5 border-x'>
         <div className='flex items-center justify-between'>
             <h1 className='p-5 pb-0 text-x font-bold'>Home</h1>
             <RefreshIcon className='mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter
             transition-all duration-500 ease-out hover:rotate-180 active:scale-125' />
+        </div>
+
+        {/*TweetBox*/}
+        <div>
+          <TweetBox />
         </div>
     </div>
   )
